98179063c2314f74a8b6767418a627dad817f5ec
DANIEL CIUCUR <ciucur.daniel14@gmail.com>
2023-11-06 19:00:05 +0200

    Update path to match current path

--- src/main/java/uvt/homework2/xml/Main.java
+++ src/main/java/uvt/homework2/xml/Main.java
@@ -6,8 +6,8 @@ import uvt.homework2.xml.validation.XMLValidator;
 import java.io.IOException;
 
 public class Main {
-    private final static String xsdPath = "/Users/daniel/IdeaProjects/boardgame-xml/boardgame.xsd";
-    private final static String xmlPath = "/Users/daniel/IdeaProjects/boardgame-xml/boardgame.xml";
+    private final static String xsdPath = "/Users/daniel/Documents/GitHub/boardgame-xml/boardgame.xsd";
+    private final static String xmlPath = "/Users/daniel/Documents/GitHub/boardgame-xml/boardgame.xml";
 
     public static void main(String[] args) throws IOException, SAXException {
         System.out.println("XML Homework 2 -  Ciucur Daniel");
